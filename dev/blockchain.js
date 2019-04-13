cf37e91bff6e1e5d8eada9e9539ca8d4efa18df5
mwalughabura <mwalughabura@gmail.com>
2019-04-13 12:45:32 +0300

    Create hash function prototype

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
@@ -35,8 +37,10 @@ Blockchain.prototype.createNewTransaction = function (amount, sender, recipient)
     return this.getLastBlock()['index'] + 1;
 }
 
-Blockchain.prototype.hashBlock = function(blockData) {
-    
+Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
 }
 
 module.exports = Blockchain;
